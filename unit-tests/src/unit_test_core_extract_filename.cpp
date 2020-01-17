a73ad3854100fecb6e443b6c7726a3e8c2fa4e26
Evgenii Sopov <mrseakg@gmail.com>
2020-01-17 13:03:39 +0700

    Added WSJCppCore::extractFilename

---
+++ unit-tests/src/unit_test_core_extract_filename.cpp
@@ -0,0 +1,51 @@
+#include "unit_test_core_extract_filename.h"
+#include <vector>
+#include <iostream>
+#include <wsjcpp_core.h>
+
+REGISTRY_UNIT_TEST(UnitTestCoreExtractFilename)
+
+UnitTestCoreExtractFilename::UnitTestCoreExtractFilename()
+    : UnitTestBase("UnitTestCoreExtractFilename") {
+    //
+}
+
+// ---------------------------------------------------------------------
+
+void UnitTestCoreExtractFilename::init() {
+    // nothing
+}
+
+// ---------------------------------------------------------------------
+
+bool UnitTestCoreExtractFilename::run() {
+    std::map<std::string, std::string> mapPaths;
+    mapPaths.insert(std::pair<std::string, std::string>("",""));
+    mapPaths.insert(std::pair<std::string, std::string>("/usr/local/bin/some","some"));
+    mapPaths.insert(std::pair<std::string, std::string>("./bin/some","some"));
+    mapPaths.insert(std::pair<std::string, std::string>(".//../bin/some","some"));
+    mapPaths.insert(std::pair<std::string, std::string>(".//../bin/some/../",""));
+    mapPaths.insert(std::pair<std::string, std::string>(".//../bin/some/..",".."));
+    mapPaths.insert(std::pair<std::string, std::string>("/usr/local/./bin/some","some"));
+    mapPaths.insert(std::pair<std::string, std::string>("/usr/local/../bin/some","some"));
+    mapPaths.insert(std::pair<std::string, std::string>("/usr/local/..//bin/some","some"));
+    mapPaths.insert(std::pair<std::string, std::string>("/usr/local/..//bin/some/.","."));
+    mapPaths.insert(std::pair<std::string, std::string>("/usr/local/..//bin/./some/",""));
+    mapPaths.insert(std::pair<std::string, std::string>("/usr/local/../../bin/./some/",""));
+    mapPaths.insert(std::pair<std::string, std::string>("/usr/local/../../../bin/./some/",""));
+    mapPaths.insert(std::pair<std::string, std::string>(".//",""));
+    std::map<std::string, std::string>::iterator it;
+
+    int nCounter = 0;
+    for ( it = mapPaths.begin(); it != mapPaths.end(); it++ ) {
+        std::string sSource = it->first;
+        std::string sExpected = it->second;
+        std::string sGot = WSJCppCore::extractFilename(sSource);
+        if (sGot != sExpected) {
+            nCounter++;
+            WSJCppLog::err(TAG, "Wrong extract filename for: '" + sSource + "'.\n\t Got: '" + sGot + "', but expected: '" + sExpected + "'");
+        }
+        
+    }
+    return nCounter == 0;
+}
---
+++ unit-tests/src/unit_test_core_extract_filename.h
@@ -0,0 +1,13 @@
+#ifndef UNIT_TEST_CORE_EXCTRACT_FILENAME_H
+#define UNIT_TEST_CORE_EXCTRACT_FILENAME_H
+
+#include <unit_tests.h>
+
+class UnitTestCoreExtractFilename : public UnitTestBase {
+    public:
+        UnitTestCoreExtractFilename();
+        virtual void init();
+        virtual bool run();
+};
+
+#endif // UNIT_TEST_CORE_EXCTRACT_FILENAME_H
