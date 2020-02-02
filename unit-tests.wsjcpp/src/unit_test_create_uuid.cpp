582b42e4740f07cd4957d8743bf7289231860c5d
Evgenii Sopov <mrseakg@gmail.com>
2020-02-02 16:01:44 +0700

    Added unit-test create uuid

---
+++ unit-tests.wsjcpp/src/unit_test_create_uuid.cpp
@@ -0,0 +1,39 @@
+#include "unit_test_create_uuid.h"
+#include <vector>
+#include <wsjcpp_core.h>
+
+REGISTRY_UNIT_TEST(UnitTestCreateUuid)
+
+UnitTestCreateUuid::UnitTestCreateUuid()
+    : UnitTestBase("UnitTestCreateUuid") {
+}
+
+// ---------------------------------------------------------------------
+
+void UnitTestCreateUuid::init() {
+    // nothing
+}
+
+// ---------------------------------------------------------------------
+
+bool UnitTestCreateUuid::run() {
+    bool bTestSuccess = true;
+    std::vector<std::string> vUuids;
+    for (int i = 0; i < 100; i++) {
+        std::string sUuid = WSJCppCore::createUuid();
+        /*if (i < 3) {
+            Log::info(TAG, "sUuid: " + sUuid);
+        }*/
+        vUuids.push_back(sUuid);
+    }
+    for (int x = 0; x < 100; x++) {
+        for (int y = 0; y < 100; y++) {
+            if (vUuids[x] == vUuids[y] && x != y) {
+                bTestSuccess = false;
+                WSJCppLog::err(TAG, "Wrong generation random uuids: \nvUuids[" + std::to_string(x)+ "] == vUuids[" + std::to_string(y) + "] == '" + vUuids[x] + "'");
+            }
+        } 
+    }
+    return bTestSuccess;
+}
+
---
+++ unit-tests.wsjcpp/src/unit_test_create_uuid.h
@@ -0,0 +1,15 @@
+#ifndef UNIT_TEST_CREATE_UUID_H
+#define UNIT_TEST_CREATE_UUID_H
+
+#include <wsjcpp_unit_tests.h>
+
+// Description: TODO
+class UnitTestCreateUuid : public UnitTestBase {
+    public:
+        UnitTestCreateUuid();
+        virtual void init();
+        virtual bool run();
+};
+
+#endif // UNIT_TEST_CREATE_UUID_H
+
