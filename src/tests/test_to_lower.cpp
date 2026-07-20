1a6b88eadd8fc7312eab744679a35c454e423622
Evgenii Sopov <mrseakg@gmail.com>
2026-07-20 09:07:43 +0700

    Redesign unit-tests for WsjcppCore::toLower / WsjcppCore::toUpper to ctest

---
+++ src/tests/test_to_lower.cpp
@@ -0,0 +1,60 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019-2026 Evgenii Sopov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+// original source-code: https://github.com/wsjcpp/wsjcpp-core
+
+#include <vector>
+#include <wsjcpp_core.h>
+
+int main() {
+  int found_errors = 0;
+  std::string error;
+
+  struct LTest {
+    LTest(
+        const std::string &source,
+        const std::string &expected
+    ) : source(source), expected(expected) {
+    };
+    std::string source;
+    std::string expected;
+  };
+  std::vector<LTest> tests;
+  tests.push_back(LTest("ABC_DEF", "abc_def"));
+
+  for (int i = 0; i < tests.size(); i++) {
+    LTest test = tests[i];
+    std::string got = WsjcppCore::toLower(test.source);
+    if (test.expected != got) {
+      found_errors++;
+      std::cerr << "Expected '" << test.expected << "', but got '" << got << "' from '" << test.source << "'" << std::endl;
+    }
+  }
+
+  if (found_errors > 0) {
+    return 1;
+  }
+  std::cout << "ok" << std::endl;
+  return found_errors;
+}
